8613c9e5f409a8ad34aad4a7b0648fb6f339955e
mansangar13 <mansangar13@alum.us.es>
2024-03-08 09:20:46 +0100

    rutas y validaciones completas

--- DeliverUS-Backend/src/controllers/validation/RestaurantValidation.js
+++ DeliverUS-Backend/src/controllers/validation/RestaurantValidation.js
@@ -1,11 +1,30 @@
 import { check } from 'express-validator'
 import { checkFileIsImage, checkFileMaxSize } from './FileValidationHelper.js'
+import { User } from '../../models/models.js'
 const maxFileSize = 2000000 // around 2Mb
+
+const checkUserExists = async (value, { req }) => {
+  try {
+    const restaurant = await User.findByPk(req.body.restaurantId)
+    if (restaurant === null) {
+      return Promise.reject(new Error('The userId does not exist.'))
+    } else { return Promise.resolve() }
+  } catch (err) {
+    return Promise.reject(new Error(err))
+  }
+}
 
 const create = [
   check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
   check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
   check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists().isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
   check('heroImage').custom((value, { req }) => {
     return checkFileIsImage(req, 'heroImage')
   }).withMessage('Please upload an image with format (jpeg, png).'),
@@ -18,12 +37,19 @@ const create = [
   check('logo').custom((value, { req }) => {
     return checkFileMaxSize(req, 'logo', maxFileSize)
   }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
-  // TODO: Complete validations
+  // TODO: Complete validations(completed)
 ]
 const update = [
   check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
   check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
   check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists().isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
   check('heroImage').custom((value, { req }) => {
     return checkFileIsImage(req, 'heroImage')
   }).withMessage('Please upload an image with format (jpeg, png).'),
@@ -36,7 +62,7 @@ const update = [
   check('logo').custom((value, { req }) => {
     return checkFileMaxSize(req, 'logo', maxFileSize)
   }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
-  // TODO: Complete validations
+  // TODO: Complete validations(completed)
 ]
 
 export { create, update }
--- DeliverUS-Backend/src/routes/RestaurantRoutes.js
+++ DeliverUS-Backend/src/routes/RestaurantRoutes.js
@@ -1,37 +1,74 @@
 import OrderController from '../controllers/OrderController.js'
 import ProductController from '../controllers/ProductController.js'
 import RestaurantController from '../controllers/RestaurantController.js'
+import { create, update } from '../controllers/validation/RestaurantValidation.js'
+import { hasRole, isLoggedIn } from '../middlewares/AuthMiddleware.js'
+import { checkEntityExists } from '../middlewares/EntityMiddleware.js'
+// import { checkProductHasNotBeenOrdered } from '../middlewares/ProductMiddleware.js'
+import { checkRestaurantOwnership, restaurantHasNoOrders } from '../middlewares/RestaurantMiddleware.js'
+import { handleValidation } from '../middlewares/ValidationHandlingMiddleware.js'
+import { Restaurant } from '../models/models.js'
+// import { handleFilesUpload } from '../middlewares/FileHandlerMiddleware.js'
 
 const loadFileRoutes = function (app) {
   app.route('/restaurants')
     .get(
       RestaurantController.index)
     .post(
-    // TODO: Add needed middlewares
+    // TODO: Add needed middlewares(completed)
+      isLoggedIn,
+      hasRole('owner'),
+      create,
+      handleValidation,
+      // checkRestaurantOwnership,
       RestaurantController.create)
 
   app.route('/restaurants/:restaurantId')
-    .get(RestaurantController.show)
+    .get(
+      checkEntityExists(Restaurant, 'restaurantId'),
+      RestaurantController.show)
     .put(
-    // TODO: Add needed middlewares
+    // TODO: Add needed middlewares(completed)
+      isLoggedIn,
+      hasRole('owner'),
+      checkEntityExists(Restaurant, 'restaurantId'),
+      checkRestaurantOwnership,
+      update,
+      handleValidation,
       RestaurantController.update)
     .delete(
-    // TODO: Add needed middlewares
+    // TODO: Add needed middlewares(completed)
+      isLoggedIn,
+      hasRole('owner'),
+      checkEntityExists(Restaurant, 'restaurantId'),
+      checkRestaurantOwnership,
+      restaurantHasNoOrders,
       RestaurantController.destroy)
 
   app.route('/restaurants/:restaurantId/orders')
     .get(
-    // TODO: Add needed middlewares
+    // TODO: Add needed middlewares(completed)
+      isLoggedIn,
+      hasRole('owner'),
+      checkEntityExists(Restaurant, 'restaurantId'),
+      checkRestaurantOwnership,
       OrderController.indexRestaurant)
 
   app.route('/restaurants/:restaurantId/products')
     .get(
-    // TODO: Add needed middlewares
+    // TODO: Add needed middlewares(completed)
+      isLoggedIn,
+      checkEntityExists(Restaurant, 'restaurantId'),
       ProductController.indexRestaurant)
 
   app.route('/restaurants/:restaurantId/analytics')
     .get(
-    // TODO: Add needed middlewares
+    // TODO: Add needed middlewares(completed)
+      isLoggedIn,
+      hasRole('owner'),
+      checkEntityExists(Restaurant, 'restaurantId'),
+      checkRestaurantOwnership,
+      handleValidation,
       OrderController.analytics)
 }
 export default loadFileRoutes
